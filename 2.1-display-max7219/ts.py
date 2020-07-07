bbf635998b9e7510c47e3302d4ebb4edcdabb602
林嘉明 Kame Lam <LKM0615@esp.edu.mo>
2020-07-07 16:57:16 +0800

    update

--- 2.1-display-max7219/ts.py
+++ 2.1-display-max7219/ts.py
@@ -1,10 +1,53 @@
 import time
 import max7219
-
 from machine import Pin, SPI
+
 spi = SPI(1, baudrate=10000000, polarity=0, phase=0)
 display = max7219.Matrix8x8(spi, Pin(15), 1)
 display.brightness(0)
+display.fill(0)
+m=[[1,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,1,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0]]
+display.pic(m)
+display.show()
+time.sleep(3)
+
+display.fill(0)
+display.icon("sHEART")
+display.show()
+time.sleep(2)
+
+display.fill(0)
+display.icon("HEART")
+display.show()
+time.sleep(2)
+
+display.fill(0)
+display.icon("YES")
+display.show()
+time.sleep(2)
+
+display.fill(0)
+display.icon("NO")
+display.show()
+time.sleep(2)
+
+display.fill(0)
+display.icon("HAPPY")
+display.show()
+time.sleep(2)
+
+display.fill(0)
+display.icon("SAD")
+display.show()
+time.sleep(2)
+
 display.fill(0)
 display.text('A',0,0,1)
 display.show()
